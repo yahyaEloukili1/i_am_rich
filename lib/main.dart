f1e718bfd8c2dd357b8d835d46c3bec31a04529e
yahya <yahya.ensas@hotmail.com>
2021-05-12 14:23:02 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+  MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+   return     MaterialApp(
+       home: Center(
+         child: Text('Hello world'),
+       )
+   );
+  }
+
+}
